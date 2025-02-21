56a2cf81d16d22bc47bb40860df0aed5cbf751c5
sbsatya82 <satyabratamahanta82@gmail.com>
2025-02-21 22:32:00 +0530

    bug fix

--- index.js
+++ index.js
@@ -14,7 +14,10 @@ const PORT = process.env.PORT || 5000;
 
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin : "*",
+  credentials: true, // allow session cookies
+}));
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
